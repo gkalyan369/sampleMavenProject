46628f3f4826ff0bf1fb9ec260451f5163a7782b
Kalyan Godavarthi <gkalyan369@gmail.com>
2018-07-14 14:59:32 +0530

    Adding the payload changes

--- src/main/java/com/kalyan/sampleMavenProject/App.java
+++ src/main/java/com/kalyan/sampleMavenProject/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World! addition of poll scm change" );
+        System.out.println( "Hello World! Removing Poll scm and adding github trigger" );
 
     }
 }
