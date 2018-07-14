9792882ce56fb1f4c0f00e73a40c58b1fc68bab7
Kalyan Godavarthi <gkalyan369@gmail.com>
2018-07-14 15:14:42 +0530

    commit message

--- src/main/java/com/kalyan/sampleMavenProject/App.java
+++ src/main/java/com/kalyan/sampleMavenProject/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World! Removing Poll scm and adding github trigger" );
+        System.out.println( "Hello World! Removing unwanted files" );
 
     }
 }
