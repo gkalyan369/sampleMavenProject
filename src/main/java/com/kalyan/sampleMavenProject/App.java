b9cfbdb57377795abbfe7b213386110a3b9d36f6
gkaly <gkaly@Kalyan>
2018-07-14 14:16:58 +0530

    push

---
+++ src/main/java/com/kalyan/sampleMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.kalyan.sampleMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is a change" );
+    }
+}
