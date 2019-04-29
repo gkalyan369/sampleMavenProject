25c9bc34c79e95c05ea43ea1a511e964d0bbfd74
Kalyan Godavarthi <gkalyan369@gmail.com>
2019-04-29 14:28:21 +0530

    changed the changes

--- src/main/java/com/kalyan/sampleMavenProject/App.java
+++ src/main/java/com/kalyan/sampleMavenProject/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World #! Removing unwanted files and trying them" );
+        System.out.println( "Hello World #! Removing unwanted files and trying them Kalyan" );
 
     }
 }
